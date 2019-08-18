829dba4988b8dfdfc3f9aadf0b15444d6d1ec7fe
grey275 <pjvanderpol@gmail.com>
2019-08-18 14:10:51 -0700

    login route returns jwt

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,16 +1,36 @@
-export const api = express => {
-  const app = express();
+import * as express from 'express';
+import * as jwt from 'jsonwebtoken';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+import * as fs from 'fs';
 
-  app.use((req, res, next) => {
-    console.log('used!');
-    next();
-  });
+import { JWT_SECRET_PATH } from './constants';
+import { UserDetails } from 'APIInterfaces/api';
+import { fstat } from 'fs';
 
-  app.post('/login', (req, res) => {
-    console.log('login route!');
-    console.log(req.body);
-    res.status(200).send();
-  });
+export const api = express();
 
-  return app;
-};
+const RSA_PRIVATE_KEY =  fs.readFileSync(JWT_SECRET_PATH);
+
+api.use(bodyParser.json());
+api.use(cors());
+
+api.post('/login', (req, res) => {
+  console.log('login route!');
+  console.log(req.body);
+  const { username, password } = req.body as UserDetails;
+
+  // TODO add validations
+  if (true) {
+    const userId = username + password;
+    const jwtBearerToken = jwt.sign({}, RSA_PRIVATE_KEY, {
+      algorithm: 'RS256',
+      expiresIn: 120,
+      subject: userId
+    });
+    console.log(jwtBearerToken);
+
+    res.json(jwtBearerToken);
+  }
+  res.status(200).send();
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,27 +4,20 @@ import * as dotenv from 'dotenv';
 import * as HttpServer from 'http';
 
 // middleware
-import * as cors from 'cors';
-import * as bodyParser from 'body-parser';
 
 import { api } from './api';
 import Sockets from './sockets';
 import { RoutesRecognized } from '@angular/router';
 
+// loads .env file into process.env
+dotenv.config({path: path.resolve('../.env')});
+
 const app = express();
 const http = HttpServer.createServer(app);
-const { PORT } = dotenv.config({path: path.resolve('../.env')}).parsed;
-
-const corsOptions = {
-  origin: 'localhost:3000'
-}
-
-
-app.use(cors());
-app.use(bodyParser.json());
+const { PORT } = process.env;
 
 Sockets(http);
-app.use('/api', api(express));
+app.use('/api', api);
 
 
 http.listen(PORT, () => {
