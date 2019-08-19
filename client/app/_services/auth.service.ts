b7c0064e40da61ff6b4d39deacbf6e27c4bf6aa9
grey275 <pjvanderpol@gmail.com>
2019-08-19 12:44:40 -0700

    remove dead code

--- client/app/_services/auth.service.ts
+++ client/app/_services/auth.service.ts
@@ -20,23 +20,5 @@ export class AuthService {
     console.log('sending request!');
     return this.http.put<UserDetails>('api/login', {username, password}, options)
     .pipe(shareReplay(1));
-      // return new Observable<any>(subscriber => {
-      //   fetch('api/login', {
-      //     method: 'PUT',
-      //     credentials: 'same-origin',
-      //     headers: {
-      //       Accept: 'application/json',
-      //       'Content-Type': 'application/json',
-      //       Cache: 'no-cache'
-      //     },
-      //     // redirect: 'manual',
-      //     body: JSON.stringify({username, password})
-      //   })
-      //     .then(response => {
-      //       console.log('response: ' , response);
-      //       subscriber.next();
-      //     });
-        // this is just the HTTP call,
-        // we still need to handle the reception of the token
   }
 }
