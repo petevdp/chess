9648620aa5155a236dee7d662efeb831456e73b8
grey275 <pjvanderpol@gmail.com>
2019-09-04 22:50:47 -0700

    change privateRoute to be a pure function

--- src/client/__helpers/AuthGuard.tsx
+++ src/client/__helpers/AuthGuard.tsx
@@ -1,4 +1,4 @@
-import { AuthService } from '../_services/auth.service';
+import { AuthService, useCurrentUser } from '../_services/auth.service';
 import React from 'react';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
 import { userInfo } from 'os';
@@ -6,26 +6,23 @@ import { useObservable } from 'rxjs-hooks';
 import { switchMap, filter } from 'rxjs/operators';
 import { Observable, empty } from 'rxjs';
 import { useStatefulObservable } from './useStatefulObservable';
+import { UserDetails } from '../../common/types';
 
 interface PrivateRouteProps extends RouteProps {
-  guardedComponent: React.ReactType;
+  GuardedComponent: React.ReactType;
+  redirectRoute: string;
+  currentUser: UserDetails | null;
 }
 
-// outputs a route component which will redirect to redirectRoute if the user isn't authenticated
-export const useAuthGuard = (
-  authService: AuthService,
-  redirectRoute: string
-): React.FC<PrivateRouteProps> => {
-  const currentUser = useStatefulObservable(authService && authService.currentUser$, null);
-
-  return ({guardedComponent: Component, ...rest}) => (
-    <Route
-      {...rest}
-      render={props => {
-        return currentUser
-          ? <Component {...props} />
-          : <Redirect to={redirectRoute} />
-      }}
-    />
-  )
-};
+export const PrivateRoute: React.FC<PrivateRouteProps> = ({
+  GuardedComponent, currentUser, redirectRoute, ...rest
+}) => (
+  <Route
+    {...rest}
+    render={props => {
+      return currentUser
+        ? <GuardedComponent {...props} />
+        : <Redirect to={redirectRoute} />
+    }}
+  />
+);
