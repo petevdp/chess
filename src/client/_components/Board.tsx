af1babd2b250a327a99f56620585a20e8dcc2093
grey275 <pjvanderpol@gmail.com>
2019-09-17 15:06:56 -0700

    add board and game component skeletons

---
+++ src/client/_components/Board.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Chessboard from 'chessboardjsx';
+
+export interface BoardProps {
+}
+
+export const Board: React.FC<BoardProps> = (props) => {
+  return <div>board, <Chessboard /></div>
+}
+
+
---
+++ src/client/_components/Game.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Board } from './Board';
+
+export interface GameProps {
+
+}
+
+export const Game: React.FC<GameProps> = (props) => {
+  return <Board />
+}
