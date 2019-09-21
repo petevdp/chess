9db0c7e34c48e113e109a24be188523cbb3ab7f4
grey275 <pjvanderpol@gmail.com>
2019-09-20 22:32:02 -0700

    add test for firstmoveEngine

---
+++ src/bots/__tests__/engines.spec.ts
@@ -0,0 +1,8 @@
+import { firstMoveEngine } from '../engines';
+import { Chess } from 'chess.js';
+
+test('firstMoveEngine only makes the first move available in move array', async () => {
+  const firstMove = new Chess().moves({verbose: true})[0];
+  const moveMade = await firstMoveEngine(new Chess());
+  expect(moveMade).toEqual(moveMade);
+})
