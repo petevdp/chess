203beaf19dfec0c3d40db24b0e433fb0802563f0
grey275 <pjvanderpol@gmail.com>
2019-09-24 11:20:28 -0700

    add randomMoveEngine for experimentation

--- src/bots/engines.ts
+++ src/bots/engines.ts
@@ -1,8 +1,14 @@
+import _ from 'lodash'
 import { MoveMaker } from '../common/gameProviders'
-import { ChessInstance } from 'chess.js'
+import { ChessInstance, Move } from 'chess.js'
 
 export type ChessEngine = MoveMaker
 
 export async function firstMoveEngine (chess: ChessInstance) {
   return chess.moves({ verbose: true })[0]
+}
+
+export async function randomMoveEngine (chess: ChessInstance) {
+  const moves = chess.moves({ verbose: true })
+  return _.sample(moves) as Move
 }
