57508dba08b26d1e97702760a4221204bdcf2b0e
grey275 <pjvanderpol@gmail.com>
2019-10-19 14:19:55 -0700

    add pagination

--- src/client/_components/AllStreams.tsx
+++ src/client/_components/AllStreams.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { LobbyService } from "../_services/lobby.service"
 import { SmallGameDisplay } from './StreamedGame'
+import _ from 'lodash'
+import { Route, Link } from 'react-router-dom'
 
 interface AllStreamsProps {
   lobbyService: LobbyService;
@@ -9,16 +11,48 @@ interface AllStreamsProps {
 function AllSmallGames ({ lobbyService }: AllStreamsProps) {
   const gameStateArr = lobbyService.useStreamedGameStates()
 
-  const streamedGames = gameStateArr.map(state => (
-    <SmallGameDisplay
-      key={state.id}
-      id={state.id}
-      gameState={state}
-    />)
-  )
+  const gamesPerPage = 8
+
+  const numOfPages = Math.ceil(gameStateArr.length / gamesPerPage)
+
+  const gamesPages = _.times(numOfPages).map(index => {
+    const startingIdx = index * gamesPerPage
+    const gamesForPage = gameStateArr.slice(startingIdx, startingIdx + gamesPerPage)
+
+    const streamedGames = gamesForPage.map(state => (
+      <SmallGameDisplay
+        key={state.id}
+        id={state.id}
+        gameState={state}
+      />
+    ))
+
+    return (
+      <Route
+        key={index + 1}
+        path={`/lobby/${index + 1}`}
+      >
+        {streamedGames}
+      </Route>
+    )
+  })
+
+  const pageLinks = _.range(1, numOfPages + 1).map(num => {
+    return (
+      <Link
+        key={num}
+        to={`/lobby/${num}`}
+      >
+        {num}
+      </Link>
+    )
+  })
+
   return (
     <section className="all-small-games">
-      {streamedGames}
+      {pageLinks}
+      {gamesPages}
+      {pageLinks}
     </section>
   )
 }
