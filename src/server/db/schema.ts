32c49fb3b277d41f591667310d8844d8a11a91e4
grey275 <pjvanderpol@gmail.com>
2019-10-27 12:34:56 -0700

    add games table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -17,4 +17,15 @@ pool.connect(async connection => {
       type varchar(36)
     )
   `)
+
+  await connection.query(sql`
+    CREATE TABLE main.games (
+      id varchar(36) PRIMARY KEY,
+      white varchar(36) NOT NULL REFERENCES main.users(id),
+      black varchar(36) NOT NULL REFERENCES main.users(id),
+      pgn text NOT NULL,
+      winner varchar(36) REFERENCES main.users(id),
+      end_reason varchar(20) NOT NULL
+    )
+  `)
 })
